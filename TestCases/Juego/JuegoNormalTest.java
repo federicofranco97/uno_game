42ddfbcdc83bff5e38c02f2550e30ebed06a2d5b
Andrés <trece920@hotmail.com>
2019-05-30 16:05:49 -0300

    creación del test para la clase JuegoNormal

---
+++ TestCases/Juego/JuegoNormalTest.java
@@ -0,0 +1,17 @@
+package Juego;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoNormalTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
