f7e7fcc20edad2059199f3810b9d3861fd2310d9
Andrés <trece920@hotmail.com>
2019-05-30 16:32:55 -0300

    se agregó el método tamañoMazo a la clase Mazo para verificar que el mazo se llene correctamente

--- TestCases/Juego/JuegoNormalTest.java
+++ TestCases/Juego/JuegoNormalTest.java
@@ -1,17 +1,39 @@
 package Juego;
 
+import Models.Mazo;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 class JuegoNormalTest {
-
+    Mazo mazo;
+    JuegoNormal juego;
+    ArrayList<Mazo> listaMazos;
     @BeforeEach
     void setUp() {
+        mazo = new Mazo();
+        juego = new JuegoNormal();
+        listaMazos = new ArrayList<>();
     }
 
     @AfterEach
     void tearDown() {
+    }
+
+    @Test
+    void llenarMazosTest (){
+        Mazo mazoPrincipal = mazo;
+        mazoPrincipal.llenarMazo();
+        Mazo mazoSecundario = mazoPrincipal;
+        listaMazos.addAll(Arrays.asList(mazoPrincipal, mazoSecundario));
+
+        Assertions.assertEquals(2, listaMazos.size());
+        Assertions.assertEquals(112, mazoPrincipal.tamañoMazo());
+
+
     }
 }
