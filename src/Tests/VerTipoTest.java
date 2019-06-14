5eb49bfa95e2763e1efff13039e72bdd9648b2dc
Alex <alexander.lehmacher@comunidad.ub.edu.ar>
2019-06-14 00:22:19 -0300

    creacion vertipotest

---
+++ src/Tests/VerTipoTest.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import Models.Carta;
+
+class VerTipoTest {
+	
+	Carta c;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testVerTipo() {
+		fail("Not yet implemented");
+	}
+
+}
